5eb8199dcd3b8ca1cef3dc65f92e5e7f369655a2
Rald Helbiro <helbirog@gmail.com>
2026-02-21 07:44:53 +0800

    feat(lab4): created skeleton functions

---
+++ lab_4/lab4.py
@@ -0,0 +1,20 @@
+def handle_prompt(prompt):
+    pass
+
+def handle_add(files):
+    pass
+
+def handle_commit(message):
+    pass
+
+def handle_push(remote, branch):
+    pass
+
+def handle_create_pr(branch, title, desc):
+    pass
+
+def run_sequential():
+    pass
+
+def run_parallel():
+    pass
